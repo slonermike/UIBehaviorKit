73f87c9fd1ec3f6e17b67767b6e443c712482a4f
Vladimir Kuskov <vladimir.kuskov@hotmail.com>
2016-06-11 01:05:24 +0300

    Color animation applicator

---
+++ Scripts/ApplyColorAnimationBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UIBehaviourKit.Animators;
+
+namespace UIBehaviourKit {
+
+    [AddComponentMenu("UIBehaviourKit/Apply color animation")]
+    public sealed class ApplyColorAnimationBehaviour : ApplySimpleAnimationBehaviour
+    {
+        [SerializeField]
+        private Gradient gradient;
+
+        protected override void ApplyAnimation(MaskableGraphic target, float delta)
+        {
+            target.color = gradient.Evaluate(delta);
+        }
+    }
+
+}
