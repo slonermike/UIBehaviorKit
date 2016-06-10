18f810dffcb1250ac0d946601d16b3e91b16c4e0
Vladimir Kuskov <vladimir.kuskov@hotmail.com>
2016-06-11 00:58:48 +0300

    Scale animation applicator

---
+++ Scripts/ApplyScaleAnimationBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UIBehaviourKit.Animators;
+
+namespace UIBehaviourKit {
+
+    [AddComponentMenu("UIBehaviorKit/Apply scale animation")]
+    public sealed class ApplyScaleAnimationBehaviour : ApplySimpleAnimationBehaviour
+    {
+        [SerializeField]
+        private Vector3 from = new Vector3(1.0f, 1.0f, 1.0f);
+        [SerializeField]
+        private Vector3 to = Vector3.zero;
+
+        protected override void ApplyAnimation(MaskableGraphic target, float delta)
+        {
+            target.transform.localScale = Vector3.Lerp(from, to, delta); 
+        }
+    }
+
+}
+
+
---
+++ Scripts/ApplySimpleAnimationBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+using UIBehaviourKit.Animators;
+
+namespace UIBehaviourKit {
+
+    public abstract class ApplySimpleAnimationBehaviour : MonoBehaviour, ISimpleAnimated {
+
+        [SerializeField]
+        private SimpleAnimatorFactory animation;
+        [SerializeField]
+        private MaskableGraphic[] targets;
+
+        private ISimpleAnimator animator;
+
+        private void Awake() {
+            if (targets == null || targets.Length == 0) {
+                var target = GetComponent<MaskableGraphic>();
+                Assert.IsNotNull(target, "Can't find suitrable target to apply animator");
+                targets = new MaskableGraphic[1];
+                targets[0] = target;
+            }
+            animator = animation.CreateSimpleAnimator();
+        }
+
+        private void Update() {
+            animator.Update(Time.deltaTime);
+            animator.Apply(this);
+        }
+
+        public void Apply(float delta) {
+            foreach (var it in targets) {
+                ApplyAnimation(it, delta);
+            }
+        }
+
+        protected abstract void ApplyAnimation(MaskableGraphic target, float delta);
+    }
+
+}
+
+
